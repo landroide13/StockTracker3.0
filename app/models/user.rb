29ac37a623c3a9798d5d629afc066acff94def38
landroide13 <lazaro02@hotmail.com>
2018-06-11 15:43:10 +1200

    Editing Registration Panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
 
   def stock_already_added?(ticker)
     stock = Stock.find_by_ticker(ticker)
